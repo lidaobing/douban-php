0a00f74396fbfd3f4cacd64ec7ab85456fb9074b
flyingww <flyingww@f29bb399-f852-0410-9328-bbdce50a1ab4>
2008-07-29 13:49:19 +0000

    7-29
    git-svn-id: http://douban-php-client.googlecode.com/svn/trunk@5 f29bb399-f852-0410-9328-bbdce50a1ab4

--- library/Zend/Gdata/client.php
+++ library/Zend/Gdata/client.php
@@ -13,11 +13,11 @@ class OAuthClient
 	protected $_token = null;
 	protected $_method = null;
 	
-	public function __construct($server = "http://www.douban.com", $key = null, $secret = null)
+	public function __construct($key = null, $secret = null, $server = 'http://www.douban.com')
 	{
 		$this->_server = $server;
 		$this->_consumer =  new OAuthConsumer($key, $secret);	
-		$this->_method = new OAuthSignatureMethod_HMAC_SHA1();
+		$this->_method = new OAuthSignatureMethod_PLAINTEXT();
 	}
 
 	public function login($key = null, $secret = null)
@@ -26,7 +26,6 @@ class OAuthClient
 			$this->_token = new OAuthToken($key, $secret);
 			return true;
 		}
-		print "1";
 		$result = $this->getRequestToken();
 		$key = $result["oauth_token"];
 		$secret = $result["oauth_token_secret"];
@@ -36,15 +35,17 @@ class OAuthClient
 		}	
 		$url = $this->getAuthorizationUrl($key,$secret);
 		print 'please paste the url in your webbrowser, complete the authorization then come back:';
-        	print $url;
+		print "\n";
+		print $url;
+		print "\n";
 		$fp = fopen("php://stdin", "r");
 		if ($fp) {
 			$line = fread($fp, 1);
 		}
 		
 		$token = $this->getAccessToken($key, $secret);
-		$key = $token[0];
-		$secret = $token[1];
+		$key = $token["oauth_token"];
+		$secret = $token["oauth_token_secret"];
         	if ($key) {
             		 $this->login($key, $secret);
 		} else {
@@ -57,17 +58,16 @@ class OAuthClient
 	{
 		$arr = explode("&", $content);
 		$size = sizeof($arr);
-		var_dump($arr);
 		$result = array();
 		if ($size == 2) {
-			$token = explode("=", $arr[0]);
-			$secret = explode("=", $arr[1]);
+			$token = explode("=", $arr[1]);
+			$secret = explode("=", $arr[0]);
 			$result["oauth_token"] = $token[1];
 			$result["oauth_token_secret"] = $secret[1];
 			$result["douban_user_id"] = null;
 		} else if ($size == 3) {
-			$token = explode("=", $arr[0]);
-			$secret = explode("=", $arr[1]);
+			$token = explode("=", $arr[1]);
+			$secret = explode("=", $arr[0]);
 			$user = explode("=", $arr[2]);
 			$result["oauth_token"] = $token[1];
 			$result["oauth_token_secret"] = $secret[1];
@@ -78,18 +78,17 @@ class OAuthClient
 
 	public function fetchToken($oauth_request = null)
 	{
-		$http_request = new HttpRequest($oauth_request->to_url(), HttpRequest::METH_GET);
+		$http_request = new HttpRequest($oauth_request->http_url(), HttpRequest::METH_GET);
+		$http_request->addHeaders($oauth_request->to_header());
 		$http_request->send();
-		$r = $httpre_quest->getResponseBody();
-		print "2";
-		print $r;
+		$r = $http_request->getResponsebody();
 		return $this->parse($r);
 		
 	}
 
 	public function getRequestToken()
 	{
-		$oauth_request = OAuthRequest::from_consumer_and_token($this->_consumer, NULL, "GET",REQUEST_TOKEN_URL);
+		$oauth_request = OAuthRequest::from_consumer_and_token($this->_consumer, NULL, NULL, self::REQUEST_TOKEN_URL);
 		$oauth_request->sign_request($this->_method, $this->_consumer, NULL);
 		return $this->fetchToken($oauth_request);
 	}
@@ -98,9 +97,12 @@ class OAuthClient
 	{
 		$parameters = array();
 		$parameters["oauth_token"] = $key;
-		$parameters["oauth_callback"] = $callback;
-		$oauth_request = new OAuthRequest('GET', AUTHORIZATION_URL, $parameters);
-
+ 		if ($callback) {
+			$parameters["oauth_callback"] = $callback;
+			$oauth_request = new OAuthRequest('GET', self::AUTHORIZATION_URL, $parameters);
+		} else {
+			$oauth_request = new OAuthRequest('GET', self::AUTHORIZATION_URL, $parameters);
+		}
 		return $oauth_request->to_url();
 	}
 
@@ -109,8 +111,8 @@ class OAuthClient
 		if ($key && $secret) {
 			$token = new OAuthToken($key, $secret);
 		}
-		$oauth_request = OAuthRequest::from_consumer_and_token($this->_consumer, $token, "GET",REQUEST_TOKEN_URL);
-		$oauth_request->sign_request($this->_mothod, $this->_consumer, $token);
+		$oauth_request = OAuthRequest::from_consumer_and_token($this->_consumer, $token, NULL, self::ACCESS_TOKEN_URL);
+		$oauth_request->sign_request($this->_method, $this->_consumer, $token);
 		return $this->fetchToken($oauth_request);
 	}
 
@@ -125,16 +127,16 @@ class OAuthClient
 
 	public function accessResource($method = null, $url = null)
 	{
-		$oauth_request = OAuthRequest::from_consumer_and_token($this->_consumer, $this->_token, "GET", $url);
-		$oauth_request->sign_request($this->_mothod, $this->_consumer, $this->_token);
-		$headers = $oauth_request.to_header();
+		$oauth_request = OAuthRequest::from_consumer_and_token($this->_consumer, $this->_token, null, $url);
+		$oauth_request->sign_request($this->_method, $this->_consumer, $this->_token);
+		$headers = $oauth_request->to_header();
 		if (($method == 'POST')||($method == 'PUT')) {
 			$headers['Content-Type'] = 'application/atom+xml; charset=utf-8';
-			$http_request = new HttpRequest($this->_server . ":80", $method);
-			$http_request->setHeaders($headers);
-			$http_request->send();
-			return $httpre_quest->getResponseBody();
 		}
+		$http_request = new HttpRequest($url, $method);
+		$http_request->setHeaders($headers);
+		$http_request->send();
+		return $http_request->getResponseBody();
 	}
 
 }
@@ -143,7 +145,8 @@ $API_KEY = '698805e0675f9cb33c9811a1361ed619';
 $SECRET = '4b3ef67ecd3ffe21';
 $client = new OAuthClient($key=$API_KEY, $secret=$SECRET);
 $client->login();
-$res = $client->access_resource('GET', 'http://api.douban.com/test?a=b&c=d').read();
-print "*********************";
+$res = $client->accessResource(HttpRequest::METH_GET, 'http://api.douban.com/test?a=b&c=d');
+print "*********************\n";
 print $res;
+print "\n";
 ?>
